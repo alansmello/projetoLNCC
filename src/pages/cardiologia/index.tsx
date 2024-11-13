e8f93dc203ea39d5ac30ca3c3291ce60b57e8426
alansmello <alansmello@gmail.com>
2024-11-13 16:06:37 -0300

    atualizações solicitadas professor Cabral

--- src/pages/cardiologia/index.tsx
+++ src/pages/cardiologia/index.tsx
@@ -1,4 +1,4 @@
-
+import Link from "next/link";
 
 
 export default function Cardiologia() {
@@ -12,6 +12,15 @@ export default function Cardiologia() {
         }}>
        
        <h1>Cardiologia</h1>
+       <h1>GRÁFICOS E TABELAS DISPONÍVEIS</h1>
+      <br></br>
+      <Link href={"/graficos_tabelas/cardiologia/internacoesPorMes"} style={{fontWeight: 'bold', fontStyle: 'italic'}}>1 – Total de internações por Mês</Link>
+      <Link href={"/graficos_tabelas/cardiologia/internacoesPorAno"} style={{fontWeight: 'bold', fontStyle: 'italic'}}>2 – Total de internações por Ano</Link>
+      <Link href={"/graficos_tabelas/cardiologia/totalRegistrosPorRegiao"} style={{fontWeight: 'bold', fontStyle: 'italic'}}>3 – Total de Registros por Região</Link>
+ 
+      <Link href={"/graficos_tabelas/cardiologia/totalRegistrosPorEstado"} style={{fontWeight: 'bold', fontStyle: 'italic'}}>4 - Total de Registros por Estado</Link>
+      <Link href={"/graficos_tabelas/cardiologia/totalEstadoOrdemDec"} style={{fontWeight: 'bold', fontStyle: 'italic'}}>5 – Total de Registros por Estado em Ordem Decrescente</Link>
+      <Link href={"/graficos_tabelas/cardiologia/totalEstadoOrdemCresc"} style={{fontWeight: 'bold', fontStyle: 'italic'}}>6 – Total de Registros por Estado em Ordem Crescente</Link>
       </div>
     );
 }
--- src/pages/ims/index.tsx
+++ src/pages/ims/index.tsx
@@ -1,5 +1,6 @@
 
 import Link from '@mui/material/Link';
+import Image from 'next/image';
 
 export default function Ims() {
   
@@ -10,9 +11,10 @@ export default function Ims() {
           flexDirection: 'column',
           minHeight: '85vh',
         }}>
-           <Link href="https://dgp.cnpq.br/dgp/espelhogrupo/4623" target="_blank" rel="noopener noreferrer">
-           <h1>LEMAS/IMS</h1>
-                  </Link>
+          
+            <Link href="https://dgp.cnpq.br/dgp/espelhogrupo/4623" target="_blank" rel="noopener noreferrer">
+              <Image src="/images/lemas.jpg" alt="OMS Logo" width={300} height={300} />
+            </Link>
        
       </div>
     );
